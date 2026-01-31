9296f3e59494fc2a10693ad6de3982485423789d
Sam <samridhsen9@gmail.com>
2026-01-31 17:37:58 +0530

    api's created

--- api/index.js
+++ api/index.js
@@ -1,27 +1,30 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import Taskrouter from './routes/Task.route.js'
 
-dotenv.config();
+dotenv.config()
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT
 
-const app = express();
+const app = express()
 
-app.use(express.json({limit: '16kb'}));    
+app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173',
-}));
+    origin: 'http://localhost:5173'
+}))
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-});
 
+// routes 
+
+app.use('/api/task', Taskrouter)
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Database connected.')
+}).catch(err => console.log('Database connection failed.', err))
 
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    console.log('Server running on port:', PORT)
+})
